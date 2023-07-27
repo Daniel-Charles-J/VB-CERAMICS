6e1105b6384b7c93b70179799095af4a12949c27
Daniel-Charles-J <80055586+Daniel-Charles-J@users.noreply.github.com>
2023-07-27 22:13:30 +0530

    Gallery and service components added and routing has completed upto this

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-
-const routes: Routes = [];
+import { ServiceComponent } from './service/service.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 const appRoutes : Routes = [
   {path : '', component : HomeComponent },
-  {path : 'about', component : AboutComponent}
+  {path : 'about', component : AboutComponent},
+  {path : 'service', component : ServiceComponent},
+  {path : 'gallery', component : GalleryComponent}
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeServiceComponent } from './home-service/home-service.component';
 import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { AboutComponent } from './about/about.component';
     HomeComponent,
     ContactComponent,
     HomeServiceComponent,
-    AboutComponent
+    AboutComponent,
+    ServiceComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
