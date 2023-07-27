f578cd86b2db466622fad91600e5ac50e1c80742
Daniel-Charles-J <80055586+Daniel-Charles-J@users.noreply.github.com>
2023-07-27 12:02:10 +0530

    initial components added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeServiceComponent } from './home-service/home-service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    HomeServiceComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/home-service/home-service.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-service',
+  templateUrl: './home-service.component.html',
+  styleUrls: ['./home-service.component.css']
+})
+export class HomeServiceComponent {
+
+}
