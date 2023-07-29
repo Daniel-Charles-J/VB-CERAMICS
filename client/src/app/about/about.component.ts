05eeb59ebef5b73164ed99179d20385850a02880
AngelinFaith <angelin.faithsheeba@gmail.com>
2023-07-29 15:21:25 +0530

    Navbar completed for about page.

--- client/src/app/about/about.component.ts
+++ client/src/app/about/about.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-
+  public profileContent: any = `Dr.VB Ceramic Research Centre (VBCRC), a division of VB Ceramic Consultants (a leading ceramic Equipment and Instrument manufacturer in India) has been developed to cater to the needs of research and development and quality control in Glass, Ceramics, Nano Science and Material Science.Under a single umbrella all the experimental facilities are available throughout the year for the students, researchers and research guides and engineers in colleges, universities and engineers employed in industries. VBCRC offers comprehensive testing of mechanical, physical, chemical and thermal properties of raw materials as well as finished products. Facilities to synthesize and fabricate from laboratory scale to final product stage and to further upgrade to larger scale industrial level are available. Final testing facility including all characterizations is also available to ensure the quality of the finished products.It has a highly competent staff who can solve all problems faced by industries and national labs as well as to offer guidance to students and faculty members.`
 }
