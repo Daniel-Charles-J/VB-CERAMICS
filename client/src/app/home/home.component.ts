93bc0110db3a54142dad951a74c0fb5c47eac912
Daniel-Charles-J <80055586+Daniel-Charles-J@users.noreply.github.com>
2023-07-31 14:22:08 +0530

    contact-us-navigation

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  scrollToSection(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
