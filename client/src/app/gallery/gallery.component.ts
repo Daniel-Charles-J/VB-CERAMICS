63e27f8da4e4c1835d8b1bb45048b0f31b83db90
AngelinFaith <angelin.faithsheeba@gmail.com>
2023-08-12 21:35:37 +0530

    Caurosel for gallery page added.

--- client/src/app/gallery/gallery.component.ts
+++ client/src/app/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-gallery',
@@ -11,5 +12,33 @@ export class GalleryComponent {
   }
   box_2_content : string[] = ['Home','About Us','Services','Gallery','Contact Us'];
   box_3_content : string[] = ['Raw Materials','Material Processing','Characterization','Heat Treatment Facility']
-  box_4_content : string[] = ['Contact Us','Help centre']
+  box_4_content : string[] = ['Contact Us','Help centre'];
+
+  public customOptions1: OwlOptions = {
+    items: 1.3,
+    loop:true,
+    autoplay:true,
+    margin: 20,
+    autoplayTimeout: 1000,
+    dots: false,
+    autoplayHoverPause:true,
+  };
+  public customOptions: OwlOptions = {
+    items: 4.5,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout: 400,
+    dots: false,
+    autoplayHoverPause:true,
+  };
+
+  public customOptions_left: OwlOptions = {
+    items:4.5,
+    loop:true,
+    rtl: true,
+    autoplay:true,
+    autoplayTimeout: 400,
+    dots: false,
+    autoplayHoverPause:true,
+  };
 }
