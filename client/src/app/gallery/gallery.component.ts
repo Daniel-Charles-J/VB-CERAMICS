c6502b47be218bed44727cfc5c1e72aa3d2001ad
AngelinFaith <angelin.faithsheeba@gmail.com>
2023-08-20 17:18:47 +0530

    Modified About page.

--- client/src/app/gallery/gallery.component.ts
+++ client/src/app/gallery/gallery.component.ts
@@ -10,21 +10,33 @@ export class GalleryComponent {
 
   constructor(){
   }
-  isLoaded: boolean = true;
-  @HostListener('window:resize', ['$event'])
   
-  onResize(event) {
-    this.updateSlidesToShow(window.innerWidth);
-  }
   box_2_content : string[] = ['Home','About Us','Services','Gallery','Contact Us'];
   box_3_content : string[] = ['Raw Materials','Material Processing','Characterization','Heat Treatment Facility']
   box_4_content : string[] = ['Contact Us','Help centre'];
+
+  public customOptions_main: OwlOptions = {
+    items: 1.1,
+    loop:true,
+    autoplay:true,
+    margin: 20,
+    autoplayTimeout: 1000,
+    dots: false,
+    autoplayHoverPause:true,
+    responsive: {
+      0:{
+        items: 1
+      },
+      750:{
+        items: 1.1
+      }
+    }
+  };
 
   public customOptions1: OwlOptions = {
     items: 1.1,
     loop:true,
     autoplay:true,
-    margin: 20,
     autoplayTimeout: 1000,
     dots: false,
     autoplayHoverPause:true,
@@ -33,7 +45,6 @@ export class GalleryComponent {
     items: 1.1,
     loop:true,
     autoplay:true,
-    margin: 20,
     autoplayTimeout: 1000,
     dots: false,
     autoplayHoverPause:true,
@@ -46,30 +57,51 @@ export class GalleryComponent {
     autoplayTimeout: 400,
     dots: false,
     autoplayHoverPause:true,
-    margin: 20
+    margin: 20,
+    responsive: {
+      0:{
+        items: 2.2
+      },
+      400:{
+        items: 2.2
+      },
+      600: {
+        items: 3.1
+      },
+      800:{
+        items: 3.5
+      },
+      1200:{
+        items: 4.5
+      }
+    }
   };
 
   public customOptions_left: OwlOptions = {
-    items:4.5,
+    items: 4.5,
     loop:true,
     rtl: true,
     autoplay:true,
     autoplayTimeout: 400,
     dots: false,
     autoplayHoverPause:true,
-    margin: 20
-  };
-
-  updateSlidesToShow(width: number) {
-    // this.isLoaded=false;
-    if (width < 700) {
-      this.customOptions1.items = 1;
-    } else {
-      this.customOptions1.items = 1.2;
+    margin: 20,
+    responsive: {
+      0:{
+        items: 2
+      },
+      400:{
+        items: 2.2
+      },
+      600: {
+        items: 3.1
+      },
+      800:{
+        items: 3.5
+      },
+      1000:{
+        items: 4.5
+      }
     }
-  }
-
-  // ngOnChanges(changes: SimpleChanges) {
-  //   console.log(changes)
-  // }
+  };
 }
