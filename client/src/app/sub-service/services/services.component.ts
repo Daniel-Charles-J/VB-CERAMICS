0a35ca72f14ad50b706a0118099fb1ab45ee932c
AngelinFaith <angelin.faithsheeba@gmail.com>
2023-08-31 01:15:08 +0530

    SubService page added.

--- client/src/app/sub-service/services/services.component.ts
+++ client/src/app/sub-service/services/services.component.ts
@@ -20,6 +20,16 @@ export class ServicesComponent {
   industries : any;
   lists : any = [];
   description : any;
+  top_para: any;
+  bottom_para: any;
+  table1_th: any;
+  table1_th2: any;
+  table1_td: any;
+  table1_td2: any;
+  table2_th: any;
+  table2_th2: any;
+  table2_td: any;
+  table2_td2: any;
 
 
   ngOnInit(): void {
@@ -35,6 +45,16 @@ export class ServicesComponent {
     this.industries = filteredData[0].industries;
     this.lists = filteredData[0].lists;
     this.description = filteredData[0].description;
+    this.top_para = filteredData[0].top_para;
+    this.bottom_para = filteredData[0].bottom_para
+    this.table1_th = filteredData[0].table1_th;
+    this.table1_th2 = filteredData[0].table1_th2;
+    this.table1_td = filteredData[0].table1_td;
+    this.table1_td2 = filteredData[0].table1_td2;
+    this.table2_th = filteredData[0].table2_th;
+    this.table2_th2 = filteredData[0].table2_th2;
+    this.table2_td = filteredData[0].table2_td;
+    this.table2_td2 = filteredData[0].table2_td2;
   }
 
   content = [
@@ -43,13 +63,24 @@ export class ServicesComponent {
       image_1 : '../../../assets/service13-sec-1.png',
       image_2 : '../../../assets/service13-sec-2.jpg',
       header : 'Heat treatment facility',
-      heading: 'Cold / Fired (MOR) Modulus of Rupture(max 3 samples), Flexural Strength',
+      heading: 'Box Furnace - up to 1400°C (5°C ramp & 3hrs dwell)',
+      description : "A Box Furnace features a vertical lift or swing out door allowing the various sized product(s) to be placed in the furnace.",
+      students : '₹3500 - Students',
+      industries : '₹5000 - Industries',
+      top_para : 'A Box Furnace features a vertical lift or swing out door allowing the various sized product(s) to be placed in the furnace. Box Furnaces are utilized for heat-treating, calcining, curing, annealing, stress relieving, preheating, tempering, and other high temperature thermal processes.',
+      bottom_para : 'This Furnaces are operated under normal atmosphere are used to subject oxides and clay materials for heat treatment , Sintering etc. ,Its operating features are : ',
+      table1_th : 'Available Space',
+      table1_th2 : '150 x 150 x 200 mm',
+      table1_td : 'Rate of Heating',
+      table1_td2 : '5°C/Min',
+      table2_th : 'Equipment',
+      table2_th2 : 'Quantity / Time',
+      table2_td : 'Box Furnace - up to 1400°C',
+      table2_td2 : '2Hrs / Working Zone:150x150x200 mm',
       title : 'Modulus of rupture (MOR) is like a three-point bend test. MOR measures the bond strength of the test specimen.',
       title_heading : '₹ - Enquire for Estimate',
-      students : '₹1500 - Students',
-      industries : '₹2000 - Industries',
-      lists : ['List1','List2','List3','List4','List5'],
-      description : "Within the complex realm of material science and thermal investigation, the dilatometer assumes a paramount role, serving as a conduit to comprehend how substances respond to alterations in temperature. This intricately designed instrument stands as a cornerstone of research, affording scientists and engineers a unique vantage point to explore the thermal properties and conduct of materials. Let's embark on an enlightening journey, unravelling the significance, operational mechanics, and diverse applications of the dilatometer.",
+      lists : ['Working Temperature up to RT to 1400°C', 'Tailor made box size as per the customer requirement', 'Silicon carbide heating elements from USA', '1°C accuracy at dwell temp', 'Rapid heating rate(1 to 20°C/min) programmable', 'Suitable for nanotechnology applications', 'Imported / indigenized Insulation', 'Indigenous VBCC Make phase control thyristor controller', 'TAIE PID programmable digital temperature indicator cum controller'],
+      
     },
 
     {
